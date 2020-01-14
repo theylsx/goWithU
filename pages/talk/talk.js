aa324bbb847993a5e0d1e3d890cd3480bcbd5392
sam5Lin <1150050235@qq.com>
2020-01-14 13:27:01 +0800

    树洞功能初步实现

--- pages/talk/talk.js
+++ pages/talk/talk.js
@@ -22,7 +22,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    msgList = [];
     wx.request({
       url: app.globalData.url + '/getChatRecord',
       method: 'POST',
